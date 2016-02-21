fbda7a2fe669b9d53108caa14e654b603dda606b
Mohamed Sathik <rmsadik.ece@gmail.com>
2016-02-22 09:44:19 +1100

    Committing second batch of laravel files.

---
+++ resources/views/page.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
+@section('content')
+	<h2>{{$name}}</h2>    
+	<p>This is my body content.</p>
+	<h2>If Statement</h2>
+	@if ($day == 'Friday')
+	    <p>Time to party</p>
+	@else
+	    <p>Time to make money</p>
+	@endif
+@endsection
