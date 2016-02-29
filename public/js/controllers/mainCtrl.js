e9ffe9a6c0a6ead933feb6940179eb4e8c63e242
Mohamed Sathik <rmsadik.ece@gmail.com>
2016-02-29 11:20:48 +1100

    added few more dom control.

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -15,9 +15,29 @@ app.controller("myCtrl", function($scope) {
 	$scope.firstName = "John";
 	$scope.lastName = "Doe";
 });
+
 app.controller('customersCtrl', function($scope, $http) {
 	$http.get("http://www.w3schools.com/angular/customers.php")
 	.then(function (response) {
 		$scope.names = response.data.records;
 	});
+});
+
+app.controller('carCtrl', function($scope) {
+    $scope.cars = [
+        {model : "Ford Mustang", color : "red"},
+        {model : "Fiat 500", color : "white"},
+        {model : "Volvo XC90", color : "black"}
+    ];
+});
+app.controller('domCtrl', function($scope) {
+	$scope.master = {firstName:"John", lastName:"Doe"};
+	$scope.user = angular.copy($scope.master);
+//    $scope.reset = function() {
+//        $scope.user = angular.copy($scope.master);
+//    };
+//    $scope.reset();
+	$scope.myClick=function(){window.alert('Hello world');};
+	$scope.myRandomClick=function(){window.alert('Hello random world');};
+	
 });
--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Laravel</title>
 
         <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
         <script src="/js/controllers/mainCtrl.js"></script> 
         <script src="/js/controllers/testJs.js"></script> 
 
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <style>
             html, body {
                 height: 100%;
@@ -23,13 +24,13 @@
             }
 
             .container {
-                text-align: center;
+                text-align: left;
                 display: table-cell;
                 vertical-align: middle;
             }
 
             .content {
-                text-align: center;
+                text-align: left;
                 display: inline-block;
             }
 
@@ -44,49 +45,85 @@
                 <div class="title">Angular</div>
             </div>
  -->       
+        	<div ng-app="myApp" ng-controller="myCtrl">
+	            <!--  Hello World -->
+                  <h1>@{{ firstName + " " + lastName }}</h1>
+	            
+	            <!--  Text input example -->
+                <div ng-app="">
+                    <p>Input something in the input box:</p>
+                    <p>Name : <input type="text" ng-model="name" placeholder="Enter name here"></p>
+                    <h1>H @{{name}}</h1>
+                </div>
+                
+	            <!--  Json input example -->
+                <div ng-app="" ng-init="names=[
+                    {name:'Jani',country:'Norway'},
+                    {name:'Hege',country:'Sweden'},
+                    {name:'Kai',country:'Denmark'}]">
+                    
+                    <ul>
+                      <li ng-repeat="x	in names">
+                        @{{ x.name + ', ' + x.country }}
+                      </li>
+                    </ul>
+                </div>    
+                
+	            <!--  $http, ng-if, filter input example -->
+                <div ng-app="" ng-controller="customersCtrl"> 
+                    <table>
+                      <tr ng-repeat="y in names">
+                        <td ng-if="$odd" style="background-color:#f1f1f1">
+                        	@{{ y.Name }}
+                        </td>
+                        <td ng-if="$even">
+                        	@{{ y.Name }}
+                        </td>
+                        <td ng-if="$odd" style="background-color:#f1f1f1">
+                        	@{{ y.City }}
+                        </td>
+                        <td ng-if="$even">
+                        	@{{ y.City }}
+                        </td>              
+                        <td ng-if="$odd" style="background-color:#f1f1f1">
+                        	@{{ y.Country | uppercase }}
+                        </td>
+                        <td ng-if="$even">
+                        	@{{ y.Country | uppercase }}
+                        </td>              
+                      </tr>
+                  </table>
+        		</div>
+        		
+	            <!--  select input input example -->
+        		<div ng-app="" ng-controller="carCtrl">
+                    <p>Select a car:</p>
+
+                    <select ng-model="selectedCar" ng-options="x.model for x in cars">
+                    </select>
+                    
+                    <h1>You selected: @{{selectedCar.model}}</h1>
+                    <p>It's color is: @{{selectedCar.color}}</p>
+                </div>
+                
+	            <!--  DOM -->
+                <div ng-app="" ng-init="hour=13;mySwitch=true;firstname='first name'" ng-controller="domCtrl">
+	                <p ng-show="hour > 12">I am visible.</p>
+	                <p ng-hide="true">I am not visible.</p>
+					<p ng-hide="false">I am visible. </p>
+					<p>
+						<button ng-click="myClick()" ng-disabled="mySwitch">Click Me!</button>
+						<button ng-dblclick="myRandomClick()" >Click Me!</button>
+					</p>
+					<formag >
+						<input type="text" ng-model="firstname">
+						<input type="text" ng-model="user.lastName">
+					</form>					
+                </div>                
+                
+        		
+            </div>
  		 </div>
- 		 
-	<div ng-app="myApp" ng-controller="myCtrl">
-          <h1>@{{ firstName + " " + lastName }}</h1>
-    
-        <div ng-app="">
-            <p>Input something in the input box:</p>
-            <p>Name : <input type="text" ng-model="name" placeholder="Enter name here"></p>
-            <h1>H @{{name}}</h1>
-        </div>
-        <div ng-app="" ng-init="names=[
-            {name:'Jani',country:'Norway'},
-            {name:'Hege',country:'Sweden'},
-            {name:'Kai',country:'Denmark'}]">
-            
-            <ul>
-              <li ng-repeat="x	in names">
-                @{{ x.name + ', ' + x.country }}
-              </li>
-            </ul>
-        </div>    
-        <div ng-app="" ng-controller="customersCtrl"> 
-            <table>
-              <tr ng-repeat="y in names">
-                <td ng-if="$odd" style="background-color:#f1f1f1">
-                	@{{ y.Name }}
-                </td>
-                <td ng-if="$even">
-                	@{{ y.Name }}
-                </td>
-                <td ng-if="$odd" style="background-color:#f1f1f1">
-                	@{{ y.Country }}
-                </td>
-                <td ng-if="$even">
-                	@{{ y.Country }}
-                </td>              
-              </tr>
-          </table>
-		</div>
-    </div>
-		           
-                       
-        
         
     
     </body>
